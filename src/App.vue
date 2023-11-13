498c3b94813c473f47b232c0c2cd8f78f0fb8709
CodeSandbox <support@codesandbox.io>
2023-11-13 18:48:41 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,256 +1,103 @@
+<script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import { ref } from "vue";
+
+const allUsers = ref([
+    {
+        "id": "6c41d80b-7743-11ee-b647-62239528fd2e",
+        "name": "gmorrison",
+        "email": "gmorrison@coade3.com",
+        "password": "",
+        "sysadmin": true,
+        "roles": [
+            {
+                "id": "33699aee-7743-11ee-b647-62239528fd2e",
+                "name": "test manager"
+            }
+        ],
+        "functions": []
+    },
+    {
+        "id": "a3b8abea-78c9-11ee-b647-62239528fd2e",
+        "name": "NotAdmin22",
+        "email": "notadmin@email.com",
+        "password": "",
+        "sysadmin": false,
+        "roles": [
+            {
+                "id": "3a4efd77-7743-11ee-b647-62239528fd2e",
+                "name": "lead tester"
+            }
+        ],
+        "functions": []
+    },
+    {
+        "id": "7e797b09-7a42-11ee-b647-62239528fd2e",
+        "name": "notadmin3bb",
+        "email": "notadmin3@email.com",
+        "password": "",
+        "sysadmin": false,
+        "roles": [
+            {
+                "id": "",
+                "name": "n/a"
+            }
+        ],
+        "functions": []
+    },
+    {
+        "id": "97adb398-7d5a-11ee-b647-62239528fd2e",
+        "name": "Kam Admin",
+        "email": "notadmin@email.com",
+        "password": "",
+        "sysadmin": true,
+        "roles": [
+            {
+                "id": "33699aee-7743-11ee-b647-62239528fd2e",
+                "name": "test manager"
+            }
+        ],
+        "functions": []
+    },
+    {
+        "id": "b384d455-7f0e-11ee-b647-62239528fd2e",
+        "name": "gmtester",
+        "email": "gmtester@xte.co.uk",
+        "password": "",
+        "sysadmin": false,
+        "roles": [
+            {
+                "id": "3cb2687f-7743-11ee-b647-62239528fd2e",
+                "name": "tester"
+            }
+        ],
+        "functions": []
+    },
+    {
+        "id": "8f6149ba-7f12-11ee-b647-62239528fd2e",
+        "name": "asdasdad",
+        "email": "asdasd@asdasd.com",
+        "password": "",
+        "sysadmin": false,
+        "roles": [
+            {
+                "id": "3a4efd77-7743-11ee-b647-62239528fd2e",
+                "name": "lead tester"
+            }
+        ],
+        "functions": []
+    }
+]);
+</script>
 
 <template>
   <div class="card">
     <DataTable
-      v-model:filters="filters"
-      :value="customers"
-      paginator
-      :rows="10"
-      dataKey="id"
-      filterDisplay="row"
-      :loading="loading"
-      :globalFilterFields="[
-        'name',
-        'country.name',
-        'representative.name',
-        'status',
-      ]"
+      :value="allUsers"
     >
-      <template #empty> No customers found. </template>
-      <template #loading> Loading customers data. Please wait. </template>
-
-      <!-- <Column field="name" header="Name" style="min-width: 12rem">
-        <template #body="{ data }">
-          {{ data.name }}
-        </template>
-        <template #filter="{ filterModel, filterCallback }">
-          <InputText
-            v-model="filterModel.value"
-            type="text"
-            @input="filterCallback()"
-            class="p-column-filter"
-            placeholder="Search by name"
-          />
-        </template>
-      </Column> -->
-
-      <Column
-        header="Country"
-        filterField="country.name"
-        style="min-width: 12rem"
-      >
-        <template #body="{ data }">
-          <div class="flex align-items-center gap-2">
-            <img
-              alt="flag"
-              src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-              :class="`flag flag-${data.country.code}`"
-              style="width: 24px"
-            />
-            <span>{{ data.country.name }}</span>
-          </div>
-        </template>
-        <template #filter="{ filterModel, filterCallback }">
-          <InputText
-            v-model="filterModel.value"
-            type="text"
-            @input="filterCallback()"
-            class="p-column-filter"
-            placeholder="Search by country"
-          />
-        </template>
-      </Column>
-
-      <!-- <Column
-        header="Agent"
-        filterField="representative"
-        :showFilterMenu="false"
-        :filterMenuStyle="{ width: '14rem' }"
-        style="min-width: 14rem"
-      >
-        <template #body="{ data }">
-          <div class="flex align-items-center gap-2">
-            <img
-              :alt="data.representative.name"
-              :src="`https://primefaces.org/cdn/primevue/images/avatar/${data.representative.image}`"
-              style="width: 32px"
-            />
-            <span>{{ data.representative.name }}</span>
-          </div>
-        </template>
-        <template #filter="{ filterModel, filterCallback }">
-          <MultiSelect
-            v-model="filterModel.value"
-            @change="filterCallback()"
-            :options="representatives"
-            optionLabel="name"
-            placeholder="Any"
-            class="p-column-filter"
-            style="min-width: 14rem"
-            :maxSelectedLabels="1"
-          >
-            <template #option="slotProps">
-              <div class="flex align-items-center gap-2">
-                <img
-                  :alt="slotProps.option.name"
-                  :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`"
-                  style="width: 32px"
-                />
-                <span>{{ slotProps.option.name }}</span>
-              </div>
-            </template>
-          </MultiSelect>
-        </template>
-      </Column>
-      <Column
-        field="status"
-        header="Status"
-        :showFilterMenu="false"
-        :filterMenuStyle="{ width: '14rem' }"
-        style="min-width: 12rem"
-      >
-        <template #body="{ data }">
-          <Tag :value="data.status" :severity="getSeverity(data.status)" />
-        </template>
-        <template #filter="{ filterModel, filterCallback }">
-          <Dropdown
-            v-model="filterModel.value"
-            @change="filterCallback()"
-            :options="statuses"
-            placeholder="Select One"
-            class="p-column-filter"
-            style="min-width: 12rem"
-            :showClear="true"
-          >
-            <template #option="slotProps">
-              <Tag
-                :value="slotProps.option"
-                :severity="getSeverity(slotProps.option)"
-              />
-            </template>
-          </Dropdown>
-        </template>
-      </Column>
-      <Column
-        field="verified"
-        header="Verified"
-        dataType="boolean"
-        style="min-width: 6rem"
-      >
-        <template #body="{ data }">
-          <i
-            class="pi"
-            :class="{
-              'pi-check-circle text-green-500': data.verified,
-              'pi-times-circle text-red-400': !data.verified,
-            }"
-          ></i>
-        </template>
-        <template #filter="{ filterModel, filterCallback }">
-          <TriStateCheckbox
-            v-model="filterModel.value"
-            @change="filterCallback()"
-          />
-        </template>
-      </Column> -->
+      <Column field="roles" header="Role" style="width: 25%" />
+      <Column field="roles[0].id" header="Role" style="width: 25%" />
     </DataTable>
   </div>
 </template>
-
-<script setup>
-import { ref, onMounted } from "vue";
-import { FilterMatchMode } from "primevue/api";
-import { CustomerService } from "@/service/CustomerService";
-
-const customers = ref();
-const filters = ref({
-  global: {
-    value: null,
-    matchMode: FilterMatchMode.CONTAINS,
-  },
-  name: {
-    value: null,
-    matchMode: FilterMatchMode.STARTS_WITH,
-  },
-  "country.name": {
-    value: null,
-    matchMode: FilterMatchMode.STARTS_WITH,
-  },
-  representative: {
-    value: null,
-    matchMode: FilterMatchMode.IN,
-  },
-  status: {
-    value: null,
-    matchMode: FilterMatchMode.EQUALS,
-  },
-  verified: {
-    value: null,
-    matchMode: FilterMatchMode.EQUALS,
-  },
-});
-const representatives = ref([
-  { name: "Amy Elsner", image: "amyelsner.png" },
-  { name: "Anna Fali", image: "annafali.png" },
-  { name: "Asiya Javayant", image: "asiyajavayant.png" },
-  { name: "Bernardo Dominic", image: "bernardodominic.png" },
-  { name: "Elwin Sharvill", image: "elwinsharvill.png" },
-  { name: "Ioni Bowcher", image: "ionibowcher.png" },
-  { name: "Ivan Magalhaes", image: "ivanmagalhaes.png" },
-  { name: "Onyama Limba", image: "onyamalimba.png" },
-  { name: "Stephen Shaw", image: "stephenshaw.png" },
-  { name: "XuXue Feng", image: "xuxuefeng.png" },
-]);
-const statuses = ref([
-  "unqualified",
-  "qualified",
-  "new",
-  "negotiation",
-  "renewal",
-  "proposal",
-]);
-const loading = ref(true);
-
-onMounted(() => {
-  CustomerService.getCustomersMedium().then((data) => {
-    customers.value = getCustomers(data);
-    loading.value = false;
-  });
-});
-
-const getCustomers = (data) => {
-  return [...(data || [])].map((d) => {
-    d.date = new Date(d.date);
-
-    return d;
-  });
-};
-const formatDate = (value) => {
-  return value.toLocaleDateString("en-US", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
-};
-const formatCurrency = (value) => {
-  return value.toLocaleString("en-US", { style: "currency", currency: "USD" });
-};
-const getSeverity = (status) => {
-  switch (status) {
-    case "unqualified":
-      return "danger";
-
-    case "qualified":
-      return "success";
-
-    case "new":
-      return "info";
-
-    case "negotiation":
-      return "warning";
-
-    case "renewal":
-      return null;
-  }
-};
-</script>
